c6a987fdea2b19289bf5107226637d21e47d5119
geetu040 <raoarmaghanshakir040@gmail.com>
2025-06-19 22:24:53 +0500

    remove everything

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="slimdoc",
-    version="1.0.0",
-    packages=find_packages(),  # find_packages(include=['slimdoc', 'slimdoc.*'])
-    url="https://github.com/marcel-lamott/SlimDoc/",
-    python_requires=">=3.10",
-)
